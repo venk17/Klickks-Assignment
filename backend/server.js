2e126ef2bc3d2a1d89f25294698c037745e2e84a
venk17 <k.venkatramanan2002@gamil.com>
2025-09-07 21:36:24 +0530

    update backend url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:3000',"https://klickks-assignment-sigma.vercel.app/",
   credentials: true
 }));
 
